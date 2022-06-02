5d17a4da41bf3369e22740f5f92f7bed8b50dc40
Quan Le <qle2@hawk.iit.edu>
2022-06-02 14:49:26 -0500

    Trial

--- stock/views.py
+++ stock/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return HttpResponse ("Hello World! This is a finance Django application about quantitative trading and stock!")
+    return HttpResponse ("Hello World! This is a finance application about quantitative trading and stock!")
