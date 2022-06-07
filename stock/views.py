63f098333995bcae1ebaf20ff9c7c536b1b88cd2
Quan Le <qle2@hawk.iit.edu>
2022-06-07 16:37:39 -0500

    Apply views

--- stock/views.py
+++ stock/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
+    # template_name = 'reprint_label/reprint_label_value.html'
+    # context = {}
+    # context.update({                
+    #     'object_type': "Hello World! This is a finance application about quantitative trading and stock!"
+    # })
+    # return render(request, template_name, context)
     return HttpResponse ("Hello World! This is a finance application about quantitative trading and stock!")
