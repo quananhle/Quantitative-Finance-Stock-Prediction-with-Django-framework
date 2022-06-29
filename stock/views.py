4ebbbdf6b55139c411230d16a8e081266a8543d2
quan.le <quan.le@fii-na.com>
2022-06-29 14:02:57 -0500

    Views

--- stock/views.py
+++ stock/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    # template_name = 'stock.html'
-    context = {}
-    context.update({                
-        'object_type': "Hello World! This is a finance application about quantitative trading and stock! Test Version Control"
-    })
-    # return render(request, template_name, context)
-    return HttpResponse (context['object_type'])
+    template_name = 'stock.html'
+
+    def get(self, *args, **kwargs):
+        context = {}
+        context.update({                
+            'object_type': "Hello World! This is a finance application about quantitative trading and stock! Test Version Control"
+        })
+        return render(self.request, self.template_name, context)
+        # return HttpResponse (context['object_type'])
