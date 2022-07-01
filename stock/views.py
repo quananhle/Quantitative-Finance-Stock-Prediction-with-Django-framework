a199fe61f11daf6d114ed023096e1f71a621afc9
quan.le <quan.le@fii-na.com>
2022-06-30 22:58:31 -0500

    Views DataLayer() check connection

--- stock/views.py
+++ stock/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from srv.database.db_actions import *
+
 
 # Create your views here.
 def index (request):
     template_name = 'stock.html'
-    # context = {}
-    # context.update({                
-    #     'object_type': "Hello World! This is a finance application about quantitative trading and stock! Test Version Control"
-    # })
 
     def get(self, *args, **kwargs):
         context = {}
+        db_obj = DataLayer()
+        db_conn = db_obj.connect()   
         context.update({                
             'object_type': "Hello World! This is a finance application about quantitative trading and stock! Test Version Control"
         })
