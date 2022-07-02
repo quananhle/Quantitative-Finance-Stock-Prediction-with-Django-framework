3dbc8c62e921742f571047182a2454e2950d3413
quan.le <quan.le@fii-na.com>
2022-07-02 17:59:27 -0500

    Populate data to frontend

--- stock/views.py
+++ stock/views.py
@@ -43,22 +43,22 @@ class StockPriceCalculator (View):
 
         sp_params = DataLayer().createparams(f"{''}")
         stock_get_info_result = DataLayer().runfunction(db_conn,'main_stock_get_info_fn', sp_params)
-
+        print (stock_get_info_result)
         #stock detailed info
         '''
-        # dynamic list size, easier for maintenance process, not ideal for operating time used
         stock_list = list()
         for i in range (len(stock_get_info_result[0])):
             stock_list.append([])
+        '''
+        '''
+        # dynamic list size, easier for maintenance process, not ideal for operating time used
         for i in range(len(stock_get_info_result)):
             for j in range(len(stock_get_info_result[i])):
                 stock_list[j].append(stock_get_info_result[i][j])
         '''
-
         '''
         # static index values, shorter running time, harder for maintenance when database function's return table changes, 
         # hence taking more time for maitenance as backend and database adjustments are both required
-        stock_list = [[],[],[],[]]      
         for i in range (len(stock_get_info_result)):
             stock_list[0].append(stock_get_info_result[i][0])
             stock_list[1].append(stock_get_info_result[i][1])
@@ -74,6 +74,10 @@ class StockPriceCalculator (View):
             stock_dict['business_type'].append(stock_get_info_result[i][3])
 
         context.update({
-            'stock_info': stock_get_info_result
+            'stock_info' : stock_get_info_result,
+            'company' : stock_dict['company'],
+            'id' : stock_dict['id'],
+            'symbol' : stock_dict['symbol'],
+            'business' : stock_dict['business_type']
         })
         return render(self.request, self.template_name, context)
