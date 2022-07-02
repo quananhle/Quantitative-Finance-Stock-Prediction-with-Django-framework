0dbc3c465b97b9738cfc3e5cf0c19de856077722
quan.le <quan.le@fii-na.com>
2022-07-02 03:43:15 -0500

    Arrange data retrieved from database

--- stock/views.py
+++ stock/views.py
@@ -43,7 +43,37 @@ class StockPriceCalculator (View):
 
         sp_params = DataLayer().createparams(f"{''}")
         stock_get_info_result = DataLayer().runfunction(db_conn,'main_stock_get_info_fn', sp_params)
-        # print (stock_get_info_result)
+
+        #stock detailed info
+        '''
+        # dynamic list size, easier for maintenance process, not ideal for operating time used
+        stock_list = list()
+        for i in range (len(stock_get_info_result[0])):
+            stock_list.append([])
+        for i in range(len(stock_get_info_result)):
+            for j in range(len(stock_get_info_result[i])):
+                stock_list[j].append(stock_get_info_result[i][j])
+        '''
+
+        '''
+        # static index values, shorter running time, harder for maintenance when database function's return table changes, 
+        # hence taking more time for maitenance as backend and database adjustments are both required
+        stock_list = [[],[],[],[]]      
+        for i in range (len(stock_get_info_result)):
+            stock_list[0].append(stock_get_info_result[i][0])
+            stock_list[1].append(stock_get_info_result[i][1])
+            stock_list[2].append(stock_get_info_result[i][2])
+            stock_list[3].append(stock_get_info_result[i][3])
+        '''
+        
+        stock_dict = {'company':[], 'id':[], 'symbol':[], 'business_type':[]}
+        for i in range (len(stock_get_info_result)):
+            stock_dict['company'].append(stock_get_info_result[i][0])
+            stock_dict['id'].append(stock_get_info_result[i][1])
+            stock_dict['symbol'].append(stock_get_info_result[i][2])
+            stock_dict['business_type'].append(stock_get_info_result[i][3])
+        print (stock_dict)
+        # print ("Stock: ", stock_list)
 
         context.update({
             'stock_info': stock_get_info_result
