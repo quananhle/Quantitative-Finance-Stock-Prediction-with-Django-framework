ae392fcd3857921a1fb1400981b6bfe2fcf9ef27
quan.le <quan.le@fii-na.com>
2022-07-01 22:16:05 -0500

    Run DB query to GET data from DB

--- stock/views.py
+++ stock/views.py
@@ -10,6 +10,25 @@ from srv.database.db_actions import *
 class StockPriceCalculator (View):
     template_name = 'stock.html'
 
+    def dispatch(self, request) -> HttpResponse:
+        if request.method == 'GET':
+            get_data = request.GET
+            if get_data.get('function') == 'populate()':
+                return self.populate()
+            else:
+                return self.get()
+        if request.method == 'POST':
+            post_data = request.POST
+            if post_data.get('function'):
+                if post_data.get('function') == 'confirm()':
+                    return self.confirm()
+                elif post_data.get('function') == 'approval()':
+                    return self.approval()
+                else:
+                    return self.get()
+            else:
+                return self.get()
+
     def get(self, *args, **kwargs):
         context = {}
         db_obj = DataLayer()
@@ -19,7 +38,10 @@ class StockPriceCalculator (View):
             context.update(
                 { 'errorMsg': 'There is a problem with Database connection, Call IT' } 
             ) 
-            return render(self.request, self.template_name, context)
+            return render(self.request, self.template_name, context) 
+
+        sp_params = DataLayer().createparams(f"{('APPL','GOOG','TSLA','AMD','NVDA')}")
+        model_manager_info = DataLayer().runfunction(db_conn,'mfg_model_manager_update_fn', sp_params)
 
         context.update({                
             'object_type': "Hello World! This is a finance application about quantitative trading and stock! Test Version Control"
